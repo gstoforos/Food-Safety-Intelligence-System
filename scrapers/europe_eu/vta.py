9d2246b2a0a11127b38eddf3d4ac9f8554236e6c
gstoforos <georgestof@gmail.com>
2026-05-08 10:38:00 +0300

    Add files via upload

--- scrapers/europe_eu/vta.py
+++ scrapers/europe_eu/vta.py
@@ -6,5 +6,5 @@ from scrapers._base import GenericGeminiScraper
 class VTAScraper(GenericGeminiScraper):
     AGENCY = "VTA (EE)"
     COUNTRY = "Estonia"
-    INDEX_URLS = ['https://pta.agri.ee/uudised']
+    INDEX_URLS = ['https://pta.agri.ee/otsing?search_term=&f%5B0%5D=type%3Anews']
     LANGUAGE = "et"
